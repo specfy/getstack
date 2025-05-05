7620c36214a067741b5b3f4e2fce53b905a41537
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-05-05 23:31:39 +0200

    ui: better header

--- apps/frontend/src/routes/category/$category.tsx
+++ apps/frontend/src/routes/category/$category.tsx
@@ -89,12 +89,14 @@ const Category: React.FC = () => {
   return (
     <div>
       <header className="mb-10 flex flex-col gap-2">
-        <div className="text-xs text-gray-400">Category</div>
-        <h2 className="flex gap-4 text-2xl font-semibold">
-          <div className="w-8 h-8 bg-neutral-100 rounded-md p-1 border">
-            <cat.icon size={22} />
+        <h2 className="flex gap-4 ">
+          <div className="w-12 h-12 bg-neutral-100 rounded-md p-1 border">
+            <cat.icon size={39} />
           </div>{' '}
-          {cat.name}
+          <div>
+            <div className="text-xs text-gray-400">Category</div>
+            <div className="text-2xl font-semibold">{cat.name}</div>
+          </div>
         </h2>
         <h3 className="text-lg text-neutral-500">{cat.description}</h3>
       </header>
--- apps/frontend/src/routes/tech/$techKey.tsx
+++ apps/frontend/src/routes/tech/$techKey.tsx
@@ -20,12 +20,14 @@ const Tech: React.FC = () => {
   return (
     <div>
       <header className="mb-10 flex flex-col gap-2">
-        <div className="text-xs text-gray-400">Technology</div>
-        <h2 className="flex gap-4 text-2xl font-semibold">
-          <div className="w-8 h-8 bg-neutral-100 rounded-md p-1 border">
+        <h2 className="flex gap-4 ">
+          <div className="w-12 h-12 bg-neutral-100 rounded-md p-1 border">
             <img src={`/favicons/${tech.key}.webp`} />
           </div>{' '}
-          {tech.name}
+          <div>
+            <div className="text-xs text-gray-400">Technology</div>
+            <div className="text-2xl font-semibold">{tech.name}</div>
+          </div>
         </h2>
       </header>
     </div>
