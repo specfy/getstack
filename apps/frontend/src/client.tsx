7f6d2ae3aee56b8d87cb21ecc02b62df0b97b279
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-05-25 15:18:35 +0200

    more

--- apps/frontend/src/client.tsx
+++ apps/frontend/src/client.tsx
@@ -1,8 +1,9 @@
 /// <reference types="vinxi/types/client" />
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/react-start'
-import { createRouter } from './router'
+import { StartClient } from '@tanstack/react-start';
+import { hydrateRoot } from 'react-dom/client';
 
-const router = createRouter()
+import { createRouter } from './router';
 
-hydrateRoot(document, <StartClient router={router} />)
+const router = createRouter();
+
+hydrateRoot(document, <StartClient router={router} />);
