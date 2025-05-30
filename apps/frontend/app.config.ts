60ee604a440115e231364ff2d2ecb371c9bb73ea
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-05-30 12:06:40 +0200

    chore: try cloudflare preset

--- apps/frontend/app.config.ts
+++ apps/frontend/app.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     appDirectory: 'src',
   },
   server: {
-    preset: 'cloudflare-module',
+    preset: 'cloudflare',
     unenv: cloudflare,
     routeRules: {
       '/favicons/**': { headers: imageHeaders },
