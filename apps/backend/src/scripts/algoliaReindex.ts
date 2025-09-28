40da2d0c4912ac56330476c1e3e734733dbbdf6b
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2025-09-28 19:41:05 +0200

    fix ts

--- apps/backend/src/scripts/algoliaReindex.ts
+++ apps/backend/src/scripts/algoliaReindex.ts
@@ -1,6 +1,9 @@
 import { reindexRepositoriesToAlgolia } from '../models/repositories.js';
+import { defaultLogger } from '../utils/logger.js';
+
+const logger = defaultLogger.child({ svc: 'algolia' });
 
 /**
  * npx tsx --env-file=.env apps/backend/src/scripts/algoliaReindex.ts
  */
-await reindexRepositoriesToAlgolia();
+await reindexRepositoriesToAlgolia(logger);
